769a0f00e22380f89e19f4b952a0dbfddd8ea579
Ziek <zieksanchez3@gmail.com>
2024-11-24 15:50:39 -0800

    Add logo and manifest

--- vite.config.ts
+++ vite.config.ts
@@ -15,25 +15,25 @@ export default defineConfig({
         theme_color: "#4D4D4D",
         icons: [
           {
-            src: "icons/manifest-icon-192.maskable.png",
+            src: "src/icons/manifest-icon-192.maskable.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any",
           },
           {
-            src: "icons/manifest-icon-192.maskable.png",
+            src: "src/icons/manifest-icon-192.maskable.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "maskable",
           },
           {
-            src: "icons/manifest-icon-512.maskable.png",
+            src: "src/icons/manifest-icon-512.maskable.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any",
           },
           {
-            src: "icons/manifest-icon-512.maskable.png",
+            src: "src/icons/manifest-icon-512.maskable.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
