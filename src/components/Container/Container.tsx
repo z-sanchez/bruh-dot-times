cc33815c0a33c9dc9b91b18cf116794fd4963089
Ziek <zieksanchez3@gmail.com>
2024-12-12 12:53:47 -0800

    Transition colors

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -4,7 +4,7 @@ import { TopNav } from "../TopNav";
 
 const Container = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="dark:bg-neutral-800 h-full ease-in-out w-full flex justify-between items-center flex-col">
+    <div className="transition-colors dark:bg-neutral-800 h-full ease-in-out w-full flex justify-between items-center flex-col">
       <TopNav />
       {children}
       {/* <BottomNav /> */}
